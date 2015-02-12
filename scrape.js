f4c75f8a0c634628f2f68dc73c3f251af162c8ad
Sean Herman <seanherman@gmail.com>
2015-02-12 01:08:05 -0500

    json dump

--- scrape.js
+++ scrape.js
@@ -1,3 +1,13 @@
+var fs = require('fs');
+var moment = require('moment');
 var vac = require('./vac/vac');
 
-vac.vacuumPromise().then(console.log);
+// vac.vacuumPromise().then(console.log);
+vac.vacuumPromise().then(function (jsonData) {
+  var filename = moment().format('X') + '.json';
+  fs.writeFile(filename, JSON.stringify(jsonData, null, 2), function (err) {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
