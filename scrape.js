132ecbdee72c88eccc87442514b1e723326d723d
Sean Herman <seanherman@gmail.com>
2015-02-12 01:22:22 -0500

    looper

--- scrape.js
+++ scrape.js
@@ -3,11 +3,15 @@ var moment = require('moment');
 var vac = require('./vac/vac');
 
 // vac.vacuumPromise().then(console.log);
-vac.vacuumPromise().then(function (jsonData) {
-  var filename = moment().format('X') + '.json';
-  fs.writeFile(filename, JSON.stringify(jsonData, null, 2), function (err) {
-    if (err) {
-      console.error(err);
-    }
+
+setInterval(function () {
+  vac.vacuumPromise().then(function (jsonData) {
+    console.log('Got JSON');
+    var filename = moment().format('X') + '.json';
+    fs.writeFile(filename, JSON.stringify(jsonData, null, 2), function (err) {
+      if (err) {
+        console.error(err);
+      }
+    });
   });
-});
+}, 60000);
