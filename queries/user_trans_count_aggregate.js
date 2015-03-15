3462cbfce01b5ea5bca9174e49b70f40dca7ae0d
Sean Herman <seanherman@gmail.com>
2015-03-15 00:05:20 -0400

    test queries

---
+++ queries/user_trans_count_aggregate.js
@@ -0,0 +1,18 @@
+db.trans.aggregate([
+  { $match: { "actor.external_id": { $exists: true } } },
+  { $match: { "transactions.0.target": { $exists: true } } },
+  { $unwind: "$transactions" },
+  { $group: {
+    _id: {
+      sender_external_id: "$actor.external_id",
+      recipient_external_id: "$transactions.target.external_id"
+    },
+    count: { $sum: 1 }
+  }},
+  { $out: "agg_user_tran_counts" }
+]);
+
+db.user_tran_counts_agg.aggregate([
+  { $sort: { count: -1, _id: 1 }},
+  { $limit: 100 }
+]);
---
+++ queries/word_count_mr.js
@@ -0,0 +1,29 @@
+var wcMap = function () {
+  var words = this.message.toLowerCase().match(/\S+/g);
+  words.forEach(function (word) {
+    if (word.length > 100) {
+      emit(word.slice(0, 100), 1);
+    } else {
+      emit(word, 1);
+    }
+  });
+};
+
+var wcReduce = function (wordKey, wordValues) {
+  var count = wordValues.reduce(function(prev, next) {
+    return prev + next;
+  }, 0);
+  return count;
+};
+
+db.trans.mapReduce(
+  wcMap,
+  wcReduce,
+  { out: {merge: "message_word_count"} }
+);
+
+// Get top 100 words
+db.mr_message_word_count.aggregate([
+    {$sort: {"value": -1}},
+    {$limit: 100}
+]);
